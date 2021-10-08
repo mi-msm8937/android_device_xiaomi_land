334be9605fbbf7ad3bd91add50b5a82956964c1c
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-10-08 08:31:27 +0800

    RevengeOS: land: Create revengeos makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_land-user \
     lineage_land-userdebug \
     lineage_land-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_land.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_land-user \
+    revengeos_land-userdebug \
+    revengeos_land-eng
