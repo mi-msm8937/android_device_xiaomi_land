818194b1253a470c01e7cffae686befe82dfafe7
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-16 16:59:02 +0800

    land: Prefix mm-qcamera-daemon with land
    To maintain compatibility with mi8937 system images

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,7 +11,7 @@ DEVICE_PATH := device/xiaomi/land
 
 # Camera
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-    /vendor/bin/mm-qcamera-daemon=23
+    /vendor/bin/land_mm-qcamera-daemon=23
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_land
