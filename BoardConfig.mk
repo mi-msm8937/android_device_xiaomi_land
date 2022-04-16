mi-msm8937/android_device_xiaomi_landtoni

land: init: Add libinit for variants handling
Change-Id: Ia990f08d046192a42c0d951a2ce8d06b8794f6f0
Co-authored-by: Prateek Chaubey <chaubeyprateek@gmail.com>

BoardConfig.mk
@@ -13,11 +13,18 @@ DEVICE_PATH := device/xiaomi/land
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /vendor/bin/mm-qcamera-daemon=23
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_land
+TARGET_RECOVERY_DEVICE_MODULES := libinit_land
+
 # Kernel
 TARGET_KERNEL_CONFIG := mi8937_defconfig
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2017-04-01
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/land/BoardConfigVendor.mk

mi-msm8937/android_device_xiaomi_land

BoardConfig.mk
@@ -13,11 +13,18 @@ DEVICE_PATH := device/xiaomi/land
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
     /vendor/bin/mm-qcamera-daemon=23
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_land
+TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_land
+
 # Kernel
 TARGET_KERNEL_CONFIG := mi8937_defconfig
 
 # Security patch level
 VENDOR_SECURITY_PATCH := 2017-04-01
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/land/BoardConfigVendor.mk